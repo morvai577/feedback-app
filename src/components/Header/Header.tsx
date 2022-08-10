468ad0fcaf6f9c52685163742f74c3beac34ed1c
morvai577 <thevaimor@icloud.com>
2022-08-10 22:06:33 +1200

    Refactor heading component to use chakra ui

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,15 @@
+import { Box, Heading } from "@chakra-ui/react";
+
 type HeaderProps = {
   text: string;
 };
 
-const headerStyles = {
-  backgroundColor: "rgba(0,0,0,0.4)",
-  color: "#ff6a95",
-};
-
 export const Header = ({ text = "" }: HeaderProps): JSX.Element => {
   return (
-    <header style={headerStyles}>
-      <div className="container">
-        <h2>{text}</h2>
-      </div>
-    </header>
+    <Box backgroundColor="#202142">
+      <Heading as="h1" size="xl" textAlign="center" color="#ff6a95" p={2}>
+        {text}
+      </Heading>
+    </Box>
   );
 };
