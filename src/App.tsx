ebaa788aac49010d7fcc5333c5ba427cb7abb105
morvai577 <thevaimor@icloud.com>
2022-08-10 21:41:05 +1200

    Add chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-// @ts-ignore
-import { Header } from "@/components/Header/Header";
+import { AboutIconLink } from "@/components/AboutIconLink/AboutIconLink";
+import { FeedbackForm } from "@/components/FeedbackForm/FeedbackForm";
 import { FeedbackList } from "@/components/FeedbackList/FeedbackList";
 import { FeedbackStats } from "@/components/FeedbackStats/FeedbackStats";
-import { FeedbackForm } from "@/components/FeedbackForm/FeedbackForm";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { AboutPage } from "@/pages/About/AboutPage";
-import { AboutIconLink } from "@/components/AboutIconLink/AboutIconLink";
+import { Header } from "@/components/Header/Header";
 import { FeedbackProvider } from "@/context/FeedbackContext";
+import { AboutPage } from "@/pages/About/AboutPage";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <FeedbackProvider>
       <BrowserRouter>
-        <Header text="Hello World" />
-        <div className="container">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <FeedbackForm />
-                  <FeedbackStats />
-                  <FeedbackList />
-                </>
-              }
-            ></Route>
-            <Route path="/about" element={<AboutPage />} />
-          </Routes>
-        </div>
-        <AboutIconLink />
+        <ChakraProvider>
+          <Header text="Hello World" />
+          <div className="container">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <FeedbackForm />
+                    <FeedbackStats />
+                    <FeedbackList />
+                  </>
+                }
+              ></Route>
+              <Route path="/about" element={<AboutPage />} />
+            </Routes>
+          </div>
+          <AboutIconLink />
+        </ChakraProvider>
       </BrowserRouter>
     </FeedbackProvider>
   );
